20fb7d654afc15a551e39f8899075799f9587b7f
Rafal Perzyna <anthilles@outlook.com>
2018-03-09 12:12:59 +0100

    Lekcja 4 - JSX

--- starter/src/components/StorePicker.js
+++ starter/src/components/StorePicker.js
@@ -2,7 +2,15 @@ import React from 'react'; // zaimportowanie modułu react
 
 class StorePicker extends React.Component { // określenie klasy naszego komponentu oraz określenie jej jako komponentu
     render(){ // każdy komponent musi mieć przynajmniej jeden moduł - render()
-        return <p>hello x2</p>
+        return (
+            <React.Fragment>
+                <form className="store-selector">
+                    <h2>Please Enter A Store</h2>
+                    <input type="text" require placeholder="Store Name"/>
+                    <button type="submit">Visit store &#8658;</button>
+                </form>
+            </React.Fragment>
+        )
     }
 }
 
