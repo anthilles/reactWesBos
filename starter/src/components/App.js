e76a84283f0bc8016a5e814894a33b8bc9ca7a8a
Rafal Perzyna <anthilles@outlook.com>
2018-03-12 11:08:58 +0100

    app.js, komponent zarządzający innymi komponentami

---
+++ starter/src/components/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Header from "./Header";
+import Order from "./Order";
+import Inventory from "./Inventory";
+
+class App extends React.Component {
+    render(){
+        return(
+            <div className="catch-of-the-day">
+                <div className="menu">
+                    <Header /> {/* komponent do napisania */}
+                </div>
+                <Order /> {/* komponent do napisania */}
+                <Inventory /> {/* komponent do napisania */}
+            </div>
+        )
+    }
+}
+
+export default App;
--- starter/src/index.js
+++ starter/src/index.js
@@ -2,6 +2,7 @@ import React from 'react'; // zaimportowanie modułu react
 import { render } from 'react-dom'; // zaimportowanie metody z modułu react.dom
 
 import StorePicker from './components/StorePicker' //musimy zaimportować komponent StorePicker
+import App from './components/App' //musimy zaimportować komponent StorePicker
 import "./css/style.css"
 
-render(<StorePicker/>, document.querySelector('#main')); //moduł w którym określamy element zwracany oraz element gdzie jest on zwracany. <StorePicker/> = <StorePicker></StorePicker>
+render(<App/>, document.querySelector('#main')); //moduł w którym określamy element zwracany oraz element gdzie jest on zwracany. <StorePicker/> = <StorePicker></StorePicker>
